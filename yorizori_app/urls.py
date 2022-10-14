23e9263e6284d041d9a1792b8dadd094cda0e6b9
hyunseo <abcdefghjk5@naver.com>
2022-10-14 15:01:19 +0900

    pathUrl

--- yorizori_app/urls.py
+++ yorizori_app/urls.py
@@ -4,9 +4,6 @@ from yorizori_app import views
 
 urlpatterns = [
     path('', views.index),
-    path('login.html/',views.login),
-    path('logout/',views.logout),
-    path('singup.html/', views.singup),
-    path('Mupdate/', views.Mupdate)
+    path('addSource/', views.addSource)
 
 ]
