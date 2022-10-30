d246660dec19d6a5474cd01e8aa2834c63f499d6
hyunseo <abcdefghjk5@naver.com>
2022-10-30 23:29:33 +0900

    로그인/회원가입 기능 수정

--- yorizori_app/views.py
+++ yorizori_app/views.py
@@ -1,55 +1,51 @@
-import re
-from sqlite3 import IntegrityError
+from aifc import Error
+import email
 from django.shortcuts import render,redirect
 from urllib import request
 from .models import *
 from django.contrib.auth.hashers import check_password
-from django.contrib.auth.models import User 
 from django.contrib import auth 
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate,login,logout
 
-
+memberinfo=MemberInfo()
 sl=[]
+res_data={}
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html' )
 
 def login(request):  #로그인 기능
+    global memberinfo
     if request.method == "POST":
-        id=request.POST.get('id')   #사용자 입력 아이디=id
-        password=request.POST.get('password')   #사용자 입력 패스워드=password  
-        res_data={}   #유효성 처리 
-        memberinfo=MemberInfo.objects.get(id=id) #MemberInfo 테이블의 id와 사용자 아이디 입력 id 일치하는 것 가져오기
-        if check_password(password, memberinfo.password): #패스워드 확인 
-            request.session['id']=memberinfo.id    #세션 유지 처리 
-            
-            user={id:memberinfo.id}
-            return  render(request, 'home.html', user)  #index 로 리다이렉트
+        u_id=request.POST.get('id')   #사용자 입력 아이디=id
+        u_password=request.POST.get('password')#사용자 입력 패스워드=password  
+        if MemberInfo.objects.filter(id=u_id).exists():
+            memberinfo = MemberInfo.objects.get(id = u_id)
+            if memberinfo.password==u_password:
+                request.session['id'] = memberinfo.id
+                return render(request,'home2.html', context={'user_is':memberinfo})
         else:
-            res_data['error']="비밀번호가 틀립니다." #에러 처리 
-            
-    return render(request,'login.html')
+            res_data['error'] = '아이디 또는 비밀번호가 일치하지 않습니다.'
+    return render(request,'login.html',res_data) 
 
-def success(request):
-    return render(request,'success.html')
         
 def logout(request):
     logout(request)
-    return render(request, 'index2.html')
+    return render(request, 'home.html')
 
 def singUp(request):
-    if request.method == 'POST':
-        id=request.POST.get('id')   #사용자 입력
-        password=request.POST.get('password')  
+    global memberinfo
+    if request.method == "POST":
+        id=request.POST.get('id')   #사용자 입력 아이디=id
+        password=request.POST.get('password')#사용자 입력 패스워드=password  
         email=request.POST.get('email')
         
-        memberinfo=MemberInfo(
-            id=id,
-            password=password,
-            email=email,
-        )
+        memberinfo=MemberInfo()
+        memberinfo.id=id 
+        memberinfo.password=password
+        memberinfo.email=email
         memberinfo.save()
-        return HttpResponseRedirect('/home/')
     return render(request, 'singUp.html')
             
 def Mupdate(request):  #회원정보 수정
@@ -104,6 +100,9 @@ def addSource(request):
 
 def search(request):
     return render(request,'search.html')
+
+def home2(request):
+    return render(request,'home2.html')
 
 
 # def login(request):
