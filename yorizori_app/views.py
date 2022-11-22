f195c589e0fee9910f497f4e38033f95f57e528a
hyunseo <abcdefghjk5@naver.com>
2022-11-22 22:33:02 +0900

    search 기능

--- yorizori_app/views.py
+++ yorizori_app/views.py
@@ -8,13 +8,13 @@ import requests
 
 memberinfo = MemberInfo()
 upload = Recipe()
+
 sl = []
 res_data = {}
 
 
 def index(request):
     return render(request, 'home.html')
-
 
 def login(request):  # 로그인 기능
     global memberinfo
@@ -87,6 +87,7 @@ def Mrecipe(request):  # 회원정보 수정
 def Upload(request):
     global upload
     global memberinfo
+    global source
     if request.method == "POST":
         memberinfo = MemberInfo()
         n_id = MemberInfo.objects.get(id=request.session['id'])
@@ -104,7 +105,12 @@ def Upload(request):
         upload.info = info
         upload.source = source
         upload.ex = ex
+        try:
+            upload.img_url=request.FILES['image']
+        except:
+            pass
         upload.save()
+
         return redirect("../MyRecipe/", request=request)
     return render(request, 'upload.html')
 
@@ -117,7 +123,7 @@ def Edit(request, recipe_id):
         recipe.source = request.POST['source']
         recipe.ex = request.POST['ex']
         recipe.save()
-        return redirect('/MyRecipe/',{'recipe':recipe})
+        return redirect('/MyRecipe/', {'recipe': recipe})
     else:
         recipe = Recipe()
         return render(request, 'update.html', {'recipe': recipe})
@@ -126,8 +132,7 @@ def Edit(request, recipe_id):
 def Delete(request, recipe_id):
     recipe = Recipe.objects.get(recipe_id=recipe_id)
     recipe.delete()
-    return redirect("/MyRecipe/", {"request":request})
-
+    return redirect("/MyRecipe/", {"request": request})
 
 
 def MyRecipe(request):
@@ -136,47 +141,96 @@ def MyRecipe(request):
 
     return render(request, 'MyRecipe.html', context)
 
-
+crolw_ = []
 def addSource(request):
-    if request.method == "POST":
-    # s=request.GET.get('s')
-    # url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query=" + s
-    # print("생성url: ",url)
-    # # ConnectionError방지
-    # headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/100.0.48496.75" }
-    # original_html = requests.get(url, headers=headers)
-    # html = BeautifulSoup(original_html.text, "html.parser")
-    # # 검색결과
-    # articles = html.select("div._list > ul.lst_total _list_base > li div.total_area > a")
-    # print(articles)
-    # #제목 가져오기
-    # title = []
-    # for i in articles:
-    #     title.append(i.attrs['title'])
+    global recipe_c
+    global crolw_
+    global s
+    global c
+    if request.method == "GET":
+        return render(request, 'addSource.html')
+    elif request.method == "POST":
+        mylist = request.POST.get("mylist")
+        new_mylist = mylist.replace('"', '').replace('[', '').replace(']', '').replace(',', '')
+        print(new_mylist)
+        url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query=" + new_mylist +"요리"
+        print("생성url: ", url)
+        # ConnectionError방지
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/100.0.48496.75"}
+        original_html = requests.get(url, headers=headers)
+        html = BeautifulSoup(original_html.text, "html.parser")
+        # 검색결과
+        articles = html.select(
+            "div._list> panel-list >div._panel>more-contents>div._more_contents_event_base>ul.lst_total>li.bx>div.total_wrap>div.total_area>a")
+        #main_pack > section > div > div._list > panel-list > div > more-contents > div > ul
+        #main_pack > section > div > div._list > panel-list > div:nth-child(1) > more-contents > div > ul
+        # print(f"articel: ============================{articles}")
+        
+        articles_img=html.select(
+            "div._list > panel-list > div > more-contents > div > ul > li > div.total_wrap > div > div.api_pcpg_wrap._freescroll_wrap > div.api_flicking_wrap.review_thumb_group._svp_content._freescroll_flicking > div:nth-child(1) > a > img")
+        # print(f"img:======================={articles_img}")
+        # 제목 가져오기
+        title = []
+        blog_content = []
+        
+        t = html.find_all("a", {"class": "api_txt_lines"})
+        for j in t:
+            title.append(j.get_text())
+            
+        # 컨텐츠 가져오기
+        c = html.find_all("a", {"class": "total_dsc _cross_trigger"})
+        for i in c:
+            blog_content.append(
+                i.find("div", {"class": "api_txt_lines"}).get_text())
 
-    # #뉴스기사 URL 가져오기
-    # url = []
-    # for i in articles:
-    #     url.append(i.attrs['href'])
+        # URL 가져오기
+        url = []
+        for i in articles:
+            url.append(i.attrs['href'])
+        
+        img = []
+        for i in articles_img:
+            img.append(i.attrs['src'])
+        # print(img)
+            
+        # title,info zip()내장함수로 리스트 합치기 
+        crolw = zip(title,blog_content,url,img)
+        crolw_= []
+        crolw_ = list(crolw)
 
-    # #뉴스기사 내용 크롤링하기
-    # contents = []
-    # for i in url:
-    #     #각 기사 html get하기
-    #     cRecipe = requests.get(i)
-    #     html = BeautifulSoup(cRecipe.text,"html.parser")
-    #     #내용 가져오기 (p태그의 내용 모두 가져오기) 
-    #     contents.append(html.find_all('p'))
-
-        return redirect("/search/")
-    return render(request,'addSource.html')
+        #반복문으로 title, info db에 insert
+        # for t,c in crolw_:
+        #     recipe_=AdminRecipe()
+        #     recipe_.title=t
+        #     recipe_.info=c
+        #     recipe_.save()
+        #db데이터 꺼내오기
+        # recipe_c=AdminRecipe.objects.all()
+        s = mylist.replace('"', '').replace('[', '').replace(']', '')
+        n_s=s.split(',')
+        for i in n_s:
+            temp = Recipe.objects.all().filter(source__contains=i).values()
+        c.clear()
+        for i in temp:
+            if set(map(lambda x: x.strip(), i["source"].split(','))) == set(n_s):
+                c.append(i)
+        return redirect("/search/", {"request": request})
 
 
 def search(request):
-    return render(request, 'search.html')
+    # print("테스트으으입니다")
+    global crolw_
+    global c
+    return render(request, 'search.html', {"recipe_c":crolw_,"c":c})
+
+def contents(request,recipe_id):
+    global c
+    content = Recipe.objects.filter(recipe_id=recipe_id).values()
+    context={"content":content}
+    return render(request,'contents.html',context)
+    
 
 
 def home2(request):
     return render(request, 'home2.html')
-
-
