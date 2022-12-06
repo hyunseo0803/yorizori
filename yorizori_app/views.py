2e89c0f87dc47300aedcee7333377da5ce114321
hyunseo <abcdefghjk5@naver.com>
2022-12-06 11:31:01 +0900

    index

--- yorizori_app/views.py
+++ yorizori_app/views.py
@@ -1,3 +1,4 @@
+from itertools import count
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from urllib import request
@@ -14,7 +15,9 @@ res_data = {}
 
 
 def index(request):
-    return render(request, 'home.html')
+    recipe= Recipe.objects.all().values()
+    return render(request, 'home.html', {"recipe": recipe})
+
 
 def login(request):  # 로그인 기능
     global memberinfo
@@ -32,7 +35,7 @@ def login(request):  # 로그인 기능
 
 
 def logout(request):
-    logout(request)
+    # session.pop()
     return render(request, 'home.html')
 
 
@@ -51,37 +54,21 @@ def singUp(request):
     return render(request, 'singUp.html')
 
 
-def Mrecipe(request):  # 회원정보 수정
-    if request.method == 'GET':
-        return render(request, 'recipe.html')
-
-    elif request.method == 'POST':
-        user = request.user
-
-        id = request.POST.get('id')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-
-        exist_id = MemberInfo.objects.filter(id=id)
-
-        # 유저의 닉네임과 같다면 그대로 저장 필터에 걸린다면 리턴 = 유저닉네임과 입력닉네임이 같아도 저장,
-
-        if exist_id and MemberInfo.id == id:
-            MemberInfo.id = id
-            MemberInfo.password = password
-            MemberInfo.email = email
-            MemberInfo.save()
-            return redirect('/', MemberInfo.id)
-
-        elif exist_id and MemberInfo.id != id:
-            return render(request, 'recipe.html', {'error': '이미 사용중인 아이디 입니다.'})
-
-        else:
-            MemberInfo.id = id
-            MemberInfo.password = password
-            MemberInfo.email = email
-            MemberInfo.save()
-            return redirect('/', MemberInfo.id)
+def editmember(request, id):  # 회원정보 수정
+    if request.method == "POST":
+        print(id)
+        member = MemberInfo.objects.get(id=id)
+        print(member.id)
+        print(member.password)
+        print(member.email)
+        # member.id = request.POST['id']
+        member.password = request.POST['password']
+        member.email = request.POST['email']
+        member.save()
+        return redirect("/home2/")
+    else:
+        member = MemberInfo()
+        return render(request, 'EditMember.html', {'id': request.session['id']})
 
 
 def Upload(request):
@@ -106,7 +93,7 @@ def Upload(request):
         upload.source = source
         upload.ex = ex
         try:
-            upload.img_url=request.FILES['image']
+            upload.img_url = request.FILES['image']
         except:
             pass
         upload.save()
@@ -141,7 +128,10 @@ def MyRecipe(request):
 
     return render(request, 'MyRecipe.html', context)
 
+
 crolw_ = []
+
+
 def addSource(request):
     global recipe_c
     global crolw_
@@ -151,9 +141,11 @@ def addSource(request):
         return render(request, 'addSource.html')
     elif request.method == "POST":
         mylist = request.POST.get("mylist")
-        new_mylist = mylist.replace('"', '').replace('[', '').replace(']', '').replace(',', '')
+        new_mylist = mylist.replace('"', '').replace(
+            '[', '').replace(']', '').replace(',', '')
         print(new_mylist)
-        url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query=" + new_mylist +"요리"
+        url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query=" + \
+            new_mylist + "요리"
         print("생성url: ", url)
         # ConnectionError방지
         headers = {
@@ -164,20 +156,20 @@ def addSource(request):
         articles = html.select(
             "div._list> panel-list >div._panel>more-contents>div._more_contents_event_base>ul.lst_total>li.bx>div.total_wrap>div.total_area>a")
         #main_pack > section > div > div._list > panel-list > div > more-contents > div > ul
-        #main_pack > section > div > div._list > panel-list > div:nth-child(1) > more-contents > div > ul
+        # main_pack > section > div > div._list > panel-list > div:nth-child(1) > more-contents > div > ul
         # print(f"articel: ============================{articles}")
-        
-        articles_img=html.select(
+
+        articles_img = html.select(
             "div._list > panel-list > div > more-contents > div > ul > li > div.total_wrap > div > div.api_pcpg_wrap._freescroll_wrap > div.api_flicking_wrap.review_thumb_group._svp_content._freescroll_flicking > div:nth-child(1) > a > img")
         # print(f"img:======================={articles_img}")
         # 제목 가져오기
         title = []
         blog_content = []
-        
+
         t = html.find_all("a", {"class": "api_txt_lines"})
         for j in t:
             title.append(j.get_text())
-            
+
         # 컨텐츠 가져오기
         c = html.find_all("a", {"class": "total_dsc _cross_trigger"})
         for i in c:
@@ -188,33 +180,34 @@ def addSource(request):
         url = []
         for i in articles:
             url.append(i.attrs['href'])
-        
+
         img = []
         for i in articles_img:
             img.append(i.attrs['src'])
         # print(img)
-            
-        # title,info zip()내장함수로 리스트 합치기 
-        crolw = zip(title,blog_content,url,img)
-        crolw_= []
+
+        # title,info zip()내장함수로 리스트 합치기
+        crolw = zip(title, blog_content, url, img)
+        crolw_ = []
         crolw_ = list(crolw)
 
-        #반복문으로 title, info db에 insert
+        # 반복문으로 title, info db에 insert
         # for t,c in crolw_:
         #     recipe_=AdminRecipe()
         #     recipe_.title=t
         #     recipe_.info=c
         #     recipe_.save()
-        #db데이터 꺼내오기
+        # db데이터 꺼내오기
         # recipe_c=AdminRecipe.objects.all()
         s = mylist.replace('"', '').replace('[', '').replace(']', '')
-        n_s=s.split(',')
+        n_s = s.split(',')
         for i in n_s:
             temp = Recipe.objects.all().filter(source__contains=i).values()
         c.clear()
         for i in temp:
-            if set(map(lambda x: x.strip(), i["source"].split(','))) == set(n_s):
+            if len(set.intersection(set(map(lambda x: x.strip(), i["source"].split(','))), set(n_s))) > 0:
                 c.append(i)
+                print(c)
         return redirect("/search/", {"request": request})
 
 
@@ -222,14 +215,14 @@ def search(request):
     # print("테스트으으입니다")
     global crolw_
     global c
-    return render(request, 'search.html', {"recipe_c":crolw_,"c":c})
+    return render(request, 'search.html', {"recipe_c": crolw_, "c": c})
 
-def contents(request,recipe_id):
+
+def contents(request, recipe_id):
     global c
     content = Recipe.objects.filter(recipe_id=recipe_id).values()
-    context={"content":content}
-    return render(request,'contents.html',context)
-    
+    context = {"content": content}
+    return render(request, 'contents.html', context)
 
 
 def home2(request):
